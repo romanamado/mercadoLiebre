fd3a3ea50d86f6667236268d1bb7df22a8c9cccb
romanamado <romanamado1@gmail.com>
2023-11-13 21:52:18 -0300

    proyecto

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const path = require("path")
 const app = express()
 const pathStatic = path.resolve(__dirname,"./public")
 
-app.use(express.static(pathStatic))
+app.use(express.static("public"))
 
 const port = process.env.PORT || 4050
 const mensaje = "servidor corriendo en el puerto "
 
-app.listen(port,()=>
-    console.log( mensaje + port)
+app.listen(port,()=>  
+    console.log( mensaje + port) 
 )
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname,"./views/home.html")
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
 
 app.get("/register",(req,res)=>{
